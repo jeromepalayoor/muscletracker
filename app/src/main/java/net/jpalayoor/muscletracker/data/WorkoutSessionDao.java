ce6abedd68668084901251e0064bb661e0593104
jeromepalayoor <me@jp-ch.gq>
2026-08-24 11:42:25 +0800

    small fix

--- app/src/main/java/net/jpalayoor/muscletracker/data/WorkoutSessionDao.java
+++ app/src/main/java/net/jpalayoor/muscletracker/data/WorkoutSessionDao.java
@@ -2,10 +2,8 @@ package net.jpalayoor.muscletracker.data;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Update;
 
 import java.util.List;
 
@@ -14,17 +12,11 @@ public interface WorkoutSessionDao {
     @Insert
     long insert(WorkoutSession session);
 
-    @Delete
-    void delete(WorkoutSession template);
-
     @Query("SELECT * FROM workout_session WHERE endTime IS NOT NULL ORDER BY startTime DESC")
     LiveData<List<WorkoutSession>> getAllSessionsLive();
 
     @Query("SELECT * FROM workout_session WHERE id = :id LIMIT 1")
     WorkoutSession getById(int id);
-
-    @Update
-    void update(WorkoutSession session);
 
     @Query("DELETE FROM workout_session WHERE id = :id")
     void deleteById(int id);
